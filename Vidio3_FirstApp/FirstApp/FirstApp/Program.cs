26336cd55646a8811a602fcc809c4d3241cbd78c
MP-SoftwareDeveloper <morteza.pourmehdi.se@gmail.com>
2024-08-31 11:48:40 +0200

    convert.tostring and convert .toint32

--- Vidio3_FirstApp/FirstApp/FirstApp/Program.cs
+++ Vidio3_FirstApp/FirstApp/FirstApp/Program.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*
- Ctrl+F5  runs the program and shows the output window
+ Ctrl+F5 (build) runs the program and shows the output window or add  Console.ReadKey(); at end of your code to see the output then use F5 (debug)to run the code
+ ctrl+K+D  align your code
  References: needed dlls  and libraries 
  solution:   project files
  App.config : project settings like netframwork version
@@ -17,11 +18,23 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int A = 3;  // A Simple data type
+            int b = 7;  // b Simple data type
+            int c;
+            string a = Convert.ToString(a);
+            string num1 = "23";
+
+            int num = Convert.ToInt32(num1);
+
+            Console.ForegroundColor = ConsoleColor.Yellow; // Console complex data type
+            Console.WriteLine(b);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("A: "+ A);
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Hello World");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
