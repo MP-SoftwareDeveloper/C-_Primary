5ff956d043210311359dd2c6aaba7c58523ad7ac
MP-SoftwareDeveloper <morteza.pourmehdi.se@gmail.com>
2024-08-31 09:48:06 +0200

    FirstApp Console colored  "Hello world" message...

---
+++ Vidio3_FirstApp/FirstApp/FirstApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Ctrl+F5  runs the program and shows the output window
+ References: needed dlls  and libraries 
+ solution:   project files
+ App.config : project settings like netframwork version
+ */
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World");
+        }
+    }
+}
